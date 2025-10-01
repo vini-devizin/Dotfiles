4d03f3ca3941aade1d22f2a8e210d0c91dfafc07
vini-devizin <viniciusdaniel.br@gmail.com>
2025-10-01 20:38:48 -0300

    A decent neovim

--- Dotfiles/nvim/lua/plugins/telescope.lua
+++ Dotfiles/nvim/lua/plugins/telescope.lua
@@ -4,5 +4,6 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
     end
 }
---
+++ Dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {"lua", "python"},
+        highlight = { enable = true },
+        indent = { enable = true }
+      })
+    end
+  }
+}
